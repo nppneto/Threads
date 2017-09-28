d5e9eb75257489009f9dd8430732b3b9cab19e9a
Nelson Neto <nelsonp2n@gmail.com>
2017-09-27 22:37:00 -0300

    Estudo com Threads.

---
+++ Threads/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads
+{
+    class App
+    {
+        public static void ImprimeK19()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("K19");
+                if (i % 10 == 0)
+                    Thread.Sleep(100);
+            }
+        }
+
+        public static void ImprimeK31()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("K31");
+                if (i % 10 == 0)
+                    Thread.Sleep(100);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread thread1 = new Thread(ImprimeK19);
+            Thread thread2 = new Thread(ImprimeK31);
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
